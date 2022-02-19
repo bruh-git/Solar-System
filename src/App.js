df1b1773dc554be94fa4498ad3b60fffec4b6c35
Bruna <brunac.cardososilva@gmail.com>
2022-02-19 14:03:47 -0300

    Requisito 9
    Em desenvolvimento
    
    Concluído.

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import SolarSystem from './components/SolarSystem';
 class App extends React.Component {
   render() {
     return (
-      <>
+      <div>
         <Header />
         <SolarSystem />
         <Missions />
-      </>
+      </div>
     );
   }
 }
--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import Title from './Title';
 import planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
+    // const { name, image } = planets;
     return (
       <div data-testid="solar-system">
-        <Title headline={ Planetas } />
+        <Title headline="Planetas" />
+        {planets.map((el) => <PlanetCard key={ el.name }
+          planetName={ el.name }
+          planetImage={ el.image }
+        />)}
       </div>
     );
   }
